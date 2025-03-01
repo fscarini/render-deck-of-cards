889fd8aaf88a5eedffa657a6ddd3deffe2ee710c
Fernando Carini <fernando2015lk@gmail.com>
2025-02-28 22:00:12 -0300

    refactor(component): troca logica de classe para logica de hooks

--- src/components/deck-of-cards/deck-of-cards.jsx
+++ src/components/deck-of-cards/deck-of-cards.jsx
@@ -1,41 +1,77 @@
-import { Component } from 'react'
+import { useEffect, useState } from 'react'
 import { getDeck, getCards } from '../../services/deck-of-cards/deck-of-cards'
 
-class DeckOfCards extends Component{
-    constructor(){
-        super()
-        this.state = {
-            cards: []
-        }
-    }
+const DeckOfCards = () => {
+    const [deck, setDeck] = useState({
+        cards: []
+    })
 
-    async componentDidMount(){
-        try{
-            const deck = await getDeck()
-            const data = await getCards(deck)
-            this.setState({
-                cards: data.cards
-            })
-        } catch(err){
-            console.error('Erro ao carregar as cartas: ', err)
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const deck = await getDeck()
+                const data = await getCards(deck)
+                setDeck({
+                    cards: data.cards
+                })
+            } catch (err) {
+                console.error('Erro ao carregar as cartas: ', err)
+            }
         }
-    }
 
-    render(){
-        return(
-            <section>
-                <ul>{
-                        this.state.cards.map((card, index) =>{
-                            return (
-                                <li key={index} style={{listStyle: "none"}}>
-                                    <img src={card.image} alt={card.value}></img>
-                                </li>
-                            )
-                        })
-                    }</ul>
-            </section>
-        )
-    }
+        fetchData()
+    }, [])
+
+    return (
+        <section>
+            <ul>{
+                deck.cards.map((card, index) => {
+                    return (
+                        <li key={index} style={{ listStyle: "none" }}>
+                            <img src={card.image} alt={card.value}></img>
+                        </li>
+                    )
+                })
+            }</ul>
+        </section>
+    )
 }
+
+// class DeckOfCards extends Component{
+//     constructor(){
+//         super()
+//         this.state = {
+//             cards: []
+//         }
+//     }
+
+//     async componentDidMount(){
+//         try{
+//             const deck = await getDeck()
+//             const data = await getCards(deck)
+//             this.setState({
+//                 cards: data.cards
+//             })
+//         } catch(err){
+//             console.error('Erro ao carregar as cartas: ', err)
+//         }
+//     }
+
+//     render(){
+//         return(
+//             <section>
+//                 <ul>{
+//                         this.state.cards.map((card, index) =>{
+//                             return (
+//                                 <li key={index} style={{listStyle: "none"}}>
+//                                     <img src={card.image} alt={card.value}></img>
+//                                 </li>
+//                             )
+//                         })
+//                     }</ul>
+//             </section>
+//         )
+//     }
+// }
 
 export default DeckOfCards
